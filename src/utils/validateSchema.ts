f114c6814b515c5cd23a508d337335102ec3ac40
gilgardosh <gilgardosh@gmail.com>
2020-09-27 17:03:16 +0300

    schema validation (ajv) as util

---
+++ src/utils/validateSchema.ts
@@ -0,0 +1,14 @@
+import Ajv from 'ajv';
+
+export async function validateSchema(jsonSchema: any, data: any, title?: string) {
+    const ajv = new Ajv({ verbose: true });
+    const valid = ajv.validate(jsonSchema, data);
+    
+    let schemaName = title? title+' ' : '';
+    console.log(`${schemaName} Schema Validation`);
+    console.log(`Valid: ${valid}`);
+    if (ajv.errors) console.log(`Errors: ${ajv.errors}`);
+    console.log("***");
+    
+    return 0
+  }
