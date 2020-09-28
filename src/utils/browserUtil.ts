3e9c1602f7b704f70bd5f3ce41ae31fc8730cde3
gilgardosh <gilgardosh@gmail.com>
2020-09-28 15:17:24 +0300

    new page util

---
+++ src/utils/browserUtil.ts
@@ -0,0 +1,16 @@
+import * as puppeteer from 'puppeteer';
+
+export async function newPage(browser: puppeteer.Browser): Promise<puppeteer.Page> {
+  // creates new page in browser
+  const VIEWPORT_WIDTH = 1024;
+  const VIEWPORT_HEIGHT = 768;
+
+  const page = await browser.newPage();
+
+  await page.setViewport({
+    width: VIEWPORT_WIDTH,
+    height: VIEWPORT_HEIGHT,
+  });
+
+  return page;
+}
