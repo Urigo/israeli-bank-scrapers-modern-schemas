ca0f6c8c4f464909dc6cfdf9c8dfcc6ce968e204
gilgardosh <gilgardosh@gmail.com>
2020-09-28 15:56:32 +0300

    update validation to return instead of print

--- src/utils/validateSchema.ts
+++ src/utils/validateSchema.ts
@@ -1,14 +1,17 @@
 import Ajv from 'ajv';
 
-export async function validateSchema(jsonSchema: any, data: any, title?: string) {
-    const ajv = new Ajv({ verbose: true });
-    const valid = ajv.validate(jsonSchema, data);
-    
-    let schemaName = title? title+' ' : '';
-    console.log(`${schemaName} Schema Validation`);
-    console.log(`Valid: ${valid}`);
-    if (ajv.errors) console.log(`Errors: ${ajv.errors}`);
-    console.log("***");
-    
-    return 0
-  }
+export async function validateSchema(
+  jsonSchema: any,
+  data: any
+) {
+  const ajv = new Ajv({ verbose: true });
+  const valid = ajv.validate(jsonSchema, data);
+
+  // let schemaName = title? title+' ' : '';
+  // console.log(`${schemaName} Schema Validation`);
+  // console.log(`Valid: ${valid}`);
+  // if (ajv.errors) console.log(`Errors: ${ajv.errors}`);
+  // console.log("***");
+
+  return { isValid: valid, errors: ajv.errors };
+}
