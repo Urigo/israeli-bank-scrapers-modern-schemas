db7059e788a8005a7ef98b461c8e4ff3372e7871
Uri Goldshtein <uri.goldshtein@gmail.com>
2020-10-17 22:51:42 +0300

    remove old comment

--- src/utils/validateSchema.ts
+++ src/utils/validateSchema.ts
@@ -3,12 +3,6 @@ import * as Ajv from 'ajv';
 export async function validateSchema(jsonSchema: any, data: any) {
   const ajv = new Ajv({ verbose: true });
   const valid = ajv.validate(jsonSchema, data);
-
-  // let schemaName = title? title+' ' : '';
-  // console.log(`${schemaName} Schema Validation`);
-  // console.log(`Valid: ${valid}`);
-  // if (ajv.errors) console.log(`Errors: ${ajv.errors}`);
-  // console.log("***");
-
+  
   return { isValid: valid, errors: ajv.errors };
 }
